4dc1814a392194d83ef41df54a9aa15212bc7fde
ccxx777 <cx2286226039@gmail.com>
2026-02-06 12:31:11 +0800

    Bundle PySide6 dependencies in build

--- build.ps1
+++ build.ps1
@@ -5,4 +5,6 @@ $env:PYTHONIOENCODING = 'utf-8'
 
 pyinstaller --noconfirm --clean --onefile --windowed `
   --name "Win11SlideshowPhotos" `
+  --collect-all PySide6 `
+  --collect-all shiboken6 `
   .\app\main.py
